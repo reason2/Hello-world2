39d71f96d52b5989554f1e51f36a88ca9a3e57ba
reason2 <richardeason2@hotmail.com>
2019-10-03 19:59:03 -0700

    Update Updating_webpage.sh

--- Updating_webpage.sh
+++ Updating_webpage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-time=$(date)
-echo "<html><body><h1>hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html
+time=$(date)                                                                                                       # Sets the variable
+echo "<html><body><h1>hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html           # Provide a output line with the updated time (time is in the UTC) and ask your status.
