f3ee52ee0f6fc34c7f7fc10555c8057369008c40
Rick Eason <richardeason2@hotmail.com>
2019-10-24 03:43:14 -0700

    Update apache-automation.sh
    Updated Apache file to reflect appropriate login and password

--- apache-automation.sh
+++ apache-automation.sh
@@ -6,14 +6,14 @@ sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                 
 sed -i '151s/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf                  # Replace AllowOverride None with AllowOverride All in the httpd.conf file
 mkdir /var/www/html/pages.dir                                                                   # Make new directory for additional webpages
 echo "<html><body><h1>Hi there NTI300</h1><p><a href="pages.dir/page2.html">Page 2</a></p></body></html>" > /var/www/html/index.html  # Create custom welcome page
-htpasswd -cb /usr/local/etc/.htpasswd REason P@ssw0rd1!                                         # Create .htaccess with user REason and in-class password.
-htpasswd -b /usr/local/etc/.htpasswd REason2 P@ssw0rd1!                                         # Create user REason2 with in-class password
+htpasswd -cb /usr/local/etc/.htpasswd R_Eason P@ssw0rd1!                                         # Create .htaccess with user R_Eason and in-class password.
+htpasswd -b /usr/local/etc/.htpasswd R_Eason2 P@ssw0rd1!                                         # Create user R_Eason2 with in-class password
 echo "AuthUserFile /usr/local/etc/.htpasswd" > /var/www/html/pages.dir/.htaccess                # Create .htaccess file
 echo 'AuthName "Page2 Access"' >> /var/www/html/pages.dir/.htaccess                             # Concatenate to .htaccess
 echo "AuthType Basic" >> /var/www/html/pages.dir/.htaccess                                      # Concatenate to .htaccess
 echo "<Limit GET POST>" >> /var/www/html/pages.dir/.htaccess                                    # Concatenate to .htaccess
-echo "require user REason" >> /var/www/html/pages.dir/.htaccess                                 # Concatenate to .htaccess
-echo "require user REason2" >> /var/www/html/pages.dir/.htaccess                                # Concatenate to .htaccess
+echo "require user R_Eason" >> /var/www/html/pages.dir/.htaccess                                 # Concatenate to .htaccess
+echo "require user R_Eason2" >> /var/www/html/pages.dir/.htaccess                                # Concatenate to .htaccess
 echo "</Limit>" >> /var/www/html/pages.dir/.htaccess                                            # Concatenate to .htaccess
 chmod 755 /var/www/html/pages.dir/.htaccess                                                     # Change .htaccess file permissions to 755
 systemctl restart httpd                                                                         # Restart apache so the changes take effect
