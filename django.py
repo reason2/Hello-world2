db75e9c069976e7be84eb27a1eec2ca53f8819e2
Rick Eason <richardeason2@hotmail.com>
2019-10-31 19:51:50 -0700

    Create django.py
    Python installing Django

---
+++ django.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+
+def setup_install():
+    print('installing pip and virtualenv so we can give django its own version of python')
+    os.system('yum -y install python-pip && pip install --upgrade pip')
+    os.system('pip install virtualenv')
+    os.chdir('/opt')
+    os.mkdir('/opt/django')
+    os.chdir('/opt/django')
+    os.system('virtualenv django-env')
+    os.system('adduser -M django && usermod -L django')
+   
+def django_install():
+    print('activating virtualenv and installing django after pre-requirements have been met')
+    os.system('source /opt/django/django-env/bin/activate && pip install django')
+    os.chdir('/opt/django')
+    os.system('source /opt/django/django-env/bin/activate ' +\
+              '&& django-admin --version ' +\
+              '&& django-admin startproject project1' +\
+              '&& chown -R django /opt/django')
+
+def django_start():
+    print('We will now commence the Django service')
+    os.chdir('/opt/django/project1')
+    os.system('source /opt/django/django-env/bin/activate ' +\
+      '&& python manage.py migrate')
+    os.system('source /opt/django/django-env/bin/activate ' +\
+      '&& echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\',\'admin@newproject.com\',\'NTI300NTI300\')" +\
+      '&& | python manage.py shell')
+    outputwithnewline = subprocess.check_output('curl -s checkip.dyndns.org | sed -e \'s/.*Current IP Address: //\' -e \'s/<.*$//\' ', shell=True)
+    print outputwithnewline
+    output = outputwithnewline.replace("\n", "")
+    old_string = "ALLOWED_HOSTS = []"
+    new_string = 'ALLOWED_HOSTS = [\'{}\']'.format(output)
+    print (old_string)
+    print (new_string)
+    with open('project1/settings.py') as f: newText=f.read().replace(old_string, new_string)
+    with open('project1/settings.py', "w") as f: f.write(newText)
+    f.close()
+    os.system('sudo -u django sh -c "source /opt/django/django-env/bin/activate && python manage.py renserver 0.0.0.0:8000&"')
+ 
+setup_install()
+django_install()
+django_start()
