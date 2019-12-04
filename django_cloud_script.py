0dedfb95df53c304b2382fc1beb1dfcfe9fa1f7a
Rick Eason <richardeason2@hotmail.com>
2019-12-04 15:17:23 -0800

    Update django_cloud_script.py

--- django_cloud_script.py
+++ django_cloud_script.py
@@ -13,7 +13,7 @@ compute = discovery.build('compute', 'v1', credentials=credentials)
 
 project = 'helical-land-254202'
 zone = 'us-central1-a'
-name = 'django-cloud-script'
+name = 'django-final'
 
 def list_instances(compute,project,zone):
   result = compute.instances().list(project=project,zone=zone).execute()
